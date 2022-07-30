d29a2e491abaed390178092514da1092f2e0bd84
CaioSSilva <caiosouzasilva13650@gmail.com>
2022-07-29 23:00:26 -0300

    Reload Fix

--- Javascript/utils.js
+++ Javascript/utils.js
@@ -9,24 +9,36 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   )
 }
+function Reload(){
+  setTimeout(()=>{
+    console.log("Reload");
+    window.location.reload()
+  },5000)
+}
 
 function determineWinner({ player, enemy, timerId }) {
   clearTimeout(timerId)
   document.querySelector('#tie').style.display = 'flex'
   if (player.health === enemy.health) {
-    document.querySelector('#tie').innerHTML = 'Tempo Esgotado!'
-    enemy.switchSprite('death')
-    player.switchSprite('death')
+    if(document.querySelector('#tie').innerHTML != 'Tempo Esgotado!'){
+      document.querySelector('#tie').innerHTML = 'Tempo Esgotado!'
+      enemy.switchSprite('death')
+      player.switchSprite('death')
+      Reload()
+    }
   } else if (player.health > enemy.health) {
-    document.querySelector('#tie').innerHTML = 'Jogador 1 Ganhou!'
-    enemy.switchSprite('death')
+    if(document.querySelector('#tie').innerHTML != 'Jogador 1 Ganhou!'){
+      document.querySelector('#tie').innerHTML = 'Jogador 1 Ganhou!'
+      enemy.switchSprite('death')
+      Reload()
+    }
   } else if (player.health < enemy.health) {
-    document.querySelector('#tie').innerHTML = 'Jogador 2 Ganhou!'
-    player.switchSprite('death')
+    if(document.querySelector('#tie').innerHTML != 'Jogador 2 Ganhou!'){
+      document.querySelector('#tie').innerHTML = 'Jogador 2 Ganhou!'
+      player.switchSprite('death')
+      Reload()
+    }
   }
-  setTimeout(()=>{
-    window.location.reload()
-  },5000)
 }
 
 let timer = 60
